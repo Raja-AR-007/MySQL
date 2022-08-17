2f9773cc92d376971c58d841369e1f7a1160ff6f
Raja-AR-007 <92211139+Raja-AR-007@users.noreply.github.com>
2022-08-17 11:19:08 +0530

    Delete Alias AS operator.sql

--- Alias AS operator.sql
+++
@@ -1,9 +0,0 @@
-                                    -- AS keyword 
--- AS keyword is used to change Columns name.
-SELECT name AS name_of_stud, major As major_of_stud
-FROM stud;
-
-                                    -- AS keyword (Company Database)
--- Find the forename and surname of all employees
-SELECT first_name AS forename, last_name AS surname
-FROM employee;
